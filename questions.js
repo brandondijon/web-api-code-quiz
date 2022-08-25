4df543a734955bfbcdfac398398258743b26b594
brandondijon <108966627+brandondijon@users.noreply.github.com>
2022-08-24 23:25:55 -0700

    questions added, readme updated

--- questions.js
+++ questions.js
@@ -1,11 +1,9 @@
-
-
 // list of questions, choices, and answers
 var questions = [
   {
     title: "Which one is a looping structure in JavaScript?",
     choices: ["All the below", "For", "While", "do-while loops"],
-    answer: "All the below"
+    answer: "All the below",
   },
   {
     title: "What are the two basic groups of data types in JavaScript?",
@@ -13,14 +11,14 @@ var questions = [
       "Primitive and attribute",
       "Primitive and reference types",
       "Reference types and attribute",
-      "None of the above"
+      "None of the above",
     ],
-    answer: "Primitive and reference types"
+    answer: "Primitive and reference types",
   },
   {
     title: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
+    answer: "alerts",
   },
   {
     title: "Boolean operators that can be used in JavaScript include:",
@@ -28,21 +26,8 @@ var questions = [
       "'And' Operator &&",
       "'Or' Operator ||",
       "'Not' Operator !",
-      "All the above"
+      "All the above",
     ],
-    answer: "All the above"
+    answer: "All the above",
   },
-  {
-    title:
-      "Which one of these is not among the three different types of errors in JavaScript?",
-    choices: [
-      "Animation time errors",
-      "Load time errors",
-      "Run time errors",
-      "Logical Errors"
-    ],
-    answer: "Animation time errors"
-  },
-  
-    
 ];
