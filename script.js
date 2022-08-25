d0ec9de602dbec71df3ff177c9d1004eb5b08577
brandondijon <108966627+brandondijon@users.noreply.github.com>
2022-08-24 22:31:20 -0700

    new score added,submit button, start button added

--- script.js
+++ script.js
@@ -118,3 +118,27 @@ function clockTick() {
     quizEnd();
   }
 }
+function saveHighscore() {
+  // get value of input box
+  var initials = initialsEl.value.trim();
+
+  if (initials !== "") {
+    // get saved scores from localstorage, or if not any, set to empty array
+    var highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    // format new score object for current user
+
+    var newScore = {
+      score: time,
+      initials: initials
+    };
+
+    // save to localstorage
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+    // redirect to next page
+    window.location.href = "score.html";
+  }
+}
